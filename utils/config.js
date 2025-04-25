3ee2825bf61f621a8fd00fc899c48aa36e8ae160
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-04-24 20:23:23 -0500

    feat: set up test environment

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI =
+  'test' === process.env.NODE_ENV
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
 const PORT = process.env.PORT
 
 module.exports = {
