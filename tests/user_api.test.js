bce22c2a8983c9c081713490a8565c8e664e35b3
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-05-02 14:25:53 -0500

    test: a invalid password is rejected

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -55,6 +55,24 @@ describe('HTTP POST', () => {
     assert.strictEqual(usersAtEnd.length, usersAtStart.length)
     assert(!usernames.includes(newUser.username))
   })
+
+  test('a user with a invalid password is rejected with statuscode 400', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'Password',
+      name: 'invalidPassword',
+      password: 'As',
+    }
+
+    await api.post('/api/users').send(newUser).expect(400)
+
+    const usersAtEnd = await helper.usersInDb()
+    const usernames = usersAtEnd.map((user) => user.username)
+
+    assert(!usernames.includes(newUser.username))
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
 })
 
 after(async () => {
