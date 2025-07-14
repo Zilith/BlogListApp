aa079288679e3fb32bc2b51b4769d62f39590811
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-07-14 16:48:29 -0500

    Add JWT token and user extraction middleware
    Introduces tokenExtractor and userExtractor middleware for handling JWT authentication. Updates errorHandler to manage JWT-related errors and MongoDB duplicate key errors. Adds new config variables for secret and user tokens.

--- utils/config.js
+++ utils/config.js
@@ -6,7 +6,15 @@ const MONGODB_URI =
     : process.env.MONGODB_URI
 const PORT = process.env.PORT
 
+const SECRET = process.env.SECRET
+
+const NORMALUSER_TOKEN = process.env.NORMALUSER_TOKEN
+const SUPERUSER_TOKEN = process.env.SUPERUSER_TOKEN
+
 module.exports = {
   MONGODB_URI,
   PORT,
+  SECRET,
+  NORMALUSER_TOKEN,
+  SUPERUSER_TOKEN,
 }
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,5 +1,30 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/users')
+const { SECRET } = require('../utils/config')
 const logger = require('./logger')
 const morgan = require('morgan')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    req.token = authorization.replace('Bearer ', '')
+  } else {
+    req.token = null
+  }
+  next()
+}
+
+const userExtractor = async (req, res, next) => {
+  const decodedToken = jwt.verify(req.token, SECRET)
+
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' })
+  } else {
+    req.user = await User.findById(decodedToken.id)
+  }
+  next()
+}
 
 morgan.token('data', function (req) {
   return JSON.stringify(req.body)
@@ -17,14 +42,26 @@ const errorHandler = (error, req, res, _next) => {
   logger.error(error.message)
 
   if (error.name === 'CastError') {
-    res.status(400).send({ error: 'malformatted id' })
+    res.status(400).json({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    res.status(400).send({ error: error.message })
+    res.status(400).json({ error: error.message })
+  } else if (
+    error.name === 'MongoServerError' &&
+    error.message.includes('E11000')
+  ) {
+    return res.status(400).json({ error: 'exprected username to be unique' })
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'invalid token' })
+  } else if (error.name === 'TokenExpiredError') {
+    return res.status(401).json({ error: 'token expired' })
   }
+  _next(error)
 }
 
 module.exports = {
   morganMiddleware,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
+  userExtractor,
 }
